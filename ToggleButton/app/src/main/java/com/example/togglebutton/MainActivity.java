268e372bd174308d9ba5757372e524da63d64af6
Italo Ramos <itaperam@gmail.com>
2020-03-30 21:03:14 -0300

    Exemplo Toggle Button

---
+++ ToggleButton/app/src/main/java/com/example/togglebutton/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.togglebutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ToggleButton toggleButton;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton = findViewById(R.id.toggleButton_id);
+        textView = findViewById(R.id.textView_exibe_id);
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                textView.setText("");
+                
+                if(isChecked){
+                    textView.setText("BOTÃO LIGADO!");
+                }
+            }
+        });
+    }
+}

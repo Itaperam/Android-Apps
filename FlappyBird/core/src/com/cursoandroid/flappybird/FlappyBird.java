59646c562b42c211505da3cd97c472af2e1c7e67
Italo Ramos <itaperam@gmail.com>
2020-04-21 17:32:30 -0300

    Captura log em das colisões

--- FlappyBird/core/src/com/cursoandroid/flappybird/FlappyBird.java
+++ FlappyBird/core/src/com/cursoandroid/flappybird/FlappyBird.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.Shape;
 
@@ -28,7 +29,7 @@ public class FlappyBird extends ApplicationAdapter {
 	private Circle passarocirculo;
 	private Rectangle retanguloCanoTopo;
 	private Rectangle retanguloCanoBaixo;
-	private ShapeRenderer shape;
+	//private ShapeRenderer shape;
 
 	//Atributos de configuração
 	private int larguraDispositivo;
@@ -52,9 +53,9 @@ public class FlappyBird extends ApplicationAdapter {
 		numeroRandomico = new Random();
 
 		passarocirculo = new Circle();
-		retanguloCanoBaixo = new Rectangle();
+		/*retanguloCanoBaixo = new Rectangle();
 		retanguloCanoTopo = new Rectangle();
-		shape = new ShapeRenderer();
+		shape = new ShapeRenderer();*/
 
 		fonte = new BitmapFont(); //Desenhando a pontuação
 		fonte.setColor(Color.WHITE);
@@ -86,72 +87,68 @@ public class FlappyBird extends ApplicationAdapter {
 
 		if (variacao > 2) variacao = 0;
 
-	    if(estadoDoJogo == 0){ //Condição de iniciar o jogo
-            if(Gdx.input.justTouched()){
-                estadoDoJogo = 1;
-            }
-        }
-	    else {
-            posicaoMovimentocanoHorizontal -= deltaTime * 200; //Movimentando os canos
-            velocidadeQueda++;
+		if (estadoDoJogo == 0) { //Condição de iniciar o jogo
+			if (Gdx.input.justTouched()) {
+				estadoDoJogo = 1;
+			}
+		} else {
+			posicaoMovimentocanoHorizontal -= deltaTime * 200; //Movimentando os canos
+			velocidadeQueda++;
 
-            if (Gdx.input.justTouched()) { // trantando o toque na tela
-                velocidadeQueda = -15; //Passaro sobe
-            }
+			if (Gdx.input.justTouched()) { // trantando o toque na tela
+				velocidadeQueda = -15; //Passaro sobe
+			}
 
-            if (posicaoInicialVertical > 0 || velocidadeQueda < 0) {
-                posicaoInicialVertical -= velocidadeQueda; //Passaro cai
-            }
+			if (posicaoInicialVertical > 0 || velocidadeQueda < 0) {
+				posicaoInicialVertical -= velocidadeQueda; //Passaro cai
+			}
 
-            //Verifica se o cano saiu completamente da tela
-            if (posicaoMovimentocanoHorizontal < -canoTopo.getWidth()) {
-                posicaoMovimentocanoHorizontal = larguraDispositivo;
-                alturaEntreCanosRandomica = numeroRandomico.nextInt(400) - 200;
+			//Verifica se o cano saiu completamente da tela
+			if (posicaoMovimentocanoHorizontal < -canoTopo.getWidth()) {
+				posicaoMovimentocanoHorizontal = larguraDispositivo;
+				alturaEntreCanosRandomica = numeroRandomico.nextInt(400) - 200;
 				marcouPonto = false;
-            }
-            //verifica pontuação
-			if(posicaoMovimentocanoHorizontal < 120){
-				if (!marcouPonto){
+			}
+			//verifica pontuação
+			if (posicaoMovimentocanoHorizontal < 120) {
+				if (!marcouPonto) {
 					pontuacao++;
 					marcouPonto = true;
 				}
 			}
-        }
+		}
 
-            batch.begin();
-            batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
-            batch.draw(canoTopo, posicaoMovimentocanoHorizontal, alturaDispositivo / 2 + espacaoEntreCanos / 2 + alturaEntreCanosRandomica);
-            batch.draw(canoBaixo, posicaoMovimentocanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacaoEntreCanos / 2 + alturaEntreCanosRandomica);
-            batch.draw(passaros[(int) variacao], 120, posicaoInicialVertical);
-            fonte.draw(batch, String.valueOf(pontuacao), larguraDispositivo - larguraDispositivo + 40, alturaDispositivo-50 );
-            batch.end();
+		batch.begin();
+		batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+		batch.draw(canoTopo, posicaoMovimentocanoHorizontal, alturaDispositivo / 2 + espacaoEntreCanos / 2 + alturaEntreCanosRandomica);
+		batch.draw(canoBaixo, posicaoMovimentocanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacaoEntreCanos / 2 + alturaEntreCanosRandomica);
+		batch.draw(passaros[(int) variacao], 120, posicaoInicialVertical);
+		fonte.draw(batch, String.valueOf(pontuacao), larguraDispositivo - larguraDispositivo + 40, alturaDispositivo - 50);
+		batch.end();
 
-            passarocirculo.set(120 + passaros[0].getWidth() / 2, posicaoInicialVertical + passaros[0].getHeight() / 2, passaros[0].getWidth() / 2);
+		passarocirculo.set(120 + passaros[0].getWidth() / 2, posicaoInicialVertical + passaros[0].getHeight() / 2, passaros[0].getWidth() / 2);
 
-            retanguloCanoBaixo = new Rectangle(
-            		posicaoMovimentocanoHorizontal, alturaDispositivo/2 - canoBaixo.getHeight() - espacaoEntreCanos/2 + alturaEntreCanosRandomica,
-					canoBaixo.getWidth(), canoBaixo.getHeight());
+		retanguloCanoBaixo = new Rectangle(
+				posicaoMovimentocanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacaoEntreCanos / 2 + alturaEntreCanosRandomica,
+				canoBaixo.getWidth(), canoBaixo.getHeight());
 
-            retanguloCanoTopo = new Rectangle(
-					posicaoMovimentocanoHorizontal, alturaDispositivo / 2 + espacaoEntreCanos / 2 + alturaEntreCanosRandomica,
-					canoTopo.getWidth(), canoTopo.getHeight());
+		retanguloCanoTopo = new Rectangle(
+				posicaoMovimentocanoHorizontal, alturaDispositivo / 2 + espacaoEntreCanos / 2 + alturaEntreCanosRandomica,
+				canoTopo.getWidth(), canoTopo.getHeight());
 
-            //Desenhar formas
-			shape.begin(ShapeRenderer.ShapeType.Filled);
+		//Desenhar formas
+		/*shape.begin(ShapeRenderer.ShapeType.Filled);
 
-			shape.circle(passarocirculo.x, passarocirculo.y, passarocirculo.radius);
-			shape.rect(retanguloCanoBaixo.x, retanguloCanoBaixo.y, retanguloCanoBaixo.width, retanguloCanoBaixo.height);
-		    shape.rect(retanguloCanoTopo.x, retanguloCanoTopo.y, retanguloCanoTopo.width, retanguloCanoTopo.height);
-			shape.setColor(Color.RED);
-			
-			shape.end();
+		shape.circle(passarocirculo.x, passarocirculo.y, passarocirculo.radius);
+		shape.rect(retanguloCanoBaixo.x, retanguloCanoBaixo.y, retanguloCanoBaixo.width, retanguloCanoBaixo.height);
+		shape.rect(retanguloCanoTopo.x, retanguloCanoTopo.y, retanguloCanoTopo.width, retanguloCanoTopo.height);
+		shape.setColor(Color.RED);
 
+		shape.end();*/
 
+		//Teste colisão
+		if(Intersector.overlaps(passarocirculo, retanguloCanoBaixo) || Intersector.overlaps(passarocirculo, retanguloCanoTopo)){
+			Gdx.app.log("Colisão", "Houve colisão");
+		}
 	}
-	
-	/*@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}*/
 }
